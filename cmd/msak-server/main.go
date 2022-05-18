10e287bc8e24e5811703b9567563d1b4e4525482
Roberto D'Auria <roberto@measurementlab.net>
2022-05-18 20:46:46 +0200

    Fix imports

--- cmd/msak-server/main.go
+++ cmd/msak-server/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/m-lab/msak/protocol"
+	internal "github.com/m-lab/msak/internal"
 )
 
 var flagEndpointCleartext = flag.String("listen", ":80", "Listen address/port for cleartext connections")
@@ -14,13 +14,13 @@ var flagEndpointCleartext = flag.String("listen", ":80", "Listen address/port fo
 func main() {
 	flag.Parse()
 	http.HandleFunc("/ndt/v7/download", func(w http.ResponseWriter, r *http.Request) {
-		if conn, err := protocol.Upgrade(w, r); err == nil {
-			protocol.HandleDownload(r.Context(), conn)
+		if conn, err := internal.Upgrade(w, r); err == nil {
+			internal.HandleDownload(r.Context(), conn)
 		}
 	})
 	http.HandleFunc("/ndt/v7/upload", func(w http.ResponseWriter, r *http.Request) {
-		if conn, err := protocol.Upgrade(w, r); err == nil {
-			protocol.HandleUpload(r.Context(), conn)
+		if conn, err := internal.Upgrade(w, r); err == nil {
+			internal.HandleUpload(r.Context(), conn)
 		}
 	})
 	go func() {
