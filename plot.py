76d60575c8e221cfb4309bddc72181caa0f3c77d
Roberto D'Auria <roberto@measurementlab.net>
2022-05-21 02:14:00 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import gzip
 import json
 import sys
 
@@ -13,14 +12,11 @@ def main():
         print("Usage: {} <input.json>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Open plain JSON file
-    print(len(sys.argv[1:])//(COLS+1)+1, COLS)
-    figure, axis = pylab.subplots(len(sys.argv[1:])//(COLS+1)+1, COLS, squeeze=False)
+    _, axis = pylab.subplots(len(sys.argv[1:])//(COLS+1)+1, COLS, squeeze=False)
     max_y = 0
     for file_idx, file in enumerate(sys.argv[1:]):
         with open(file, 'r') as f:
             data = json.load(f)
-            # Read each array of data
             for idx, _ in enumerate(data):
                 x, y = [0], [0]
                 for point in data[idx]:
@@ -28,11 +24,11 @@ def main():
                     y.append(point["rate"])
                     if point["rate"] > max_y:
                         max_y = point["rate"]
-                print(file_idx//COLS, file_idx%COLS)
                 axis[file_idx//COLS,file_idx%COLS].plot(x, y, "x-", label="stream {}".format(idx))
                 axis[file_idx//COLS,file_idx%COLS].set_xlabel("time (s)")
                 axis[file_idx//COLS,file_idx%COLS].set_ylabel("rate (B/s)")
                 axis[file_idx//COLS,file_idx%COLS].legend()
+
     for x in axis:
         for y in x:
             y.set_ylim(0, max_y)
