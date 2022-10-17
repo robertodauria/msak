4c690cccad033b0493e9d2e43a39c6c9e805623b
Roberto D'Auria <roberto@measurementlab.net>
2022-10-17 16:52:21 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,72 +1,76 @@
 #!/usr/bin/env python3
 
+import gzip
 import json
 import sys
 import os
-import matplotlib
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
 
-import pylab
+from dateutil import parser
+
+def get_json_by_mid(folder, mid):
+    res = []
+    for file in os.listdir(folder):
+        with gzip.open(os.path.join(folder, file), 'r') as fin:
+            data = json.loads(fin.read())
+            if data.get("MeasurementID") == mid:
+                res.append(data)
+    return res
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: {} <input folder 1> <input folder 2>".format(sys.argv[0]))
-        sys.exit(1)
+    if len(sys.argv) < 3:
+       print("Usage: {} <input folder> <measurement id>".format(sys.argv[0]))
+       sys.exit(1)
+    
+    root_dir = sys.argv[1]
+    mid = sys.argv[2]
+    json_data = get_json_by_mid(root_dir, mid)
 
-    fig, axes = pylab.subplots(3, 2, sharey="all")
-    # Set horizontal spacing
-    fig.subplots_adjust(hspace=0.5)
+    start_times = [parser.parse(file.get("StartTime")) for file in json_data]
+    global_start_time = min(start_times)
 
-    # Read all folders in each folder passed as argument
-    for col, root_dir in enumerate(sys.argv[1:]):
-        nstream_dirs = os.listdir(root_dir)
-        nstream_dirs.sort()
-        # Create a subplot for each folder. 
-        for row, nstreams_dir in enumerate(nstream_dirs):
-            files = os.listdir(os.path.join(root_dir, nstreams_dir))
-            files.sort()
-            for i, file in enumerate(files):
-                if file.startswith("aggregate"):
-                    with open(os.path.join(root_dir, nstreams_dir, file)) as f:
-                        data = json.load(f)
-                        times = [float(x) for x in data.keys()]
-                        axes[row, col].plot(times, data.values(), label="aggregate")
-                else: 
-                    with open(os.path.join(root_dir, nstreams_dir, file)) as f:
-                        data = json.load(f)
-                        # Get the x,y values from the data
-                        avg_x, avg_y = [], []
-                        rate_x, rate_y = [], []
-                        previous = None
-                        for measurement in data.get("Download").get("ServerMeasurements"):
-                            time = measurement.get("TCPInfo").get("ElapsedTime") / 1000000
-                            rate = measurement.get("TCPInfo").get("BytesAcked") / time / 1000000 * 8
-                            avg_x.append(time)
-                            avg_y.append(rate)
-                            if previous is not None:
-                                dtime = measurement.get("TCPInfo").get("ElapsedTime") - previous.get("TCPInfo").get("ElapsedTime")
-                                dbytes = measurement.get("TCPInfo").get("BytesAcked") - previous.get("TCPInfo").get("BytesAcked")
-                                rate_x.append(time)
-                                rate_y.append(dbytes / dtime * 8)
-                            previous = measurement
+    plot_data = {}
+    for idx, file in enumerate(json_data):
+        x_values, y_values = [], []
+        start_time = parser.parse(file.get("StartTime"))
+        start_time_offset = start_time - global_start_time
+        print("flow #{} time offset: {} us".format(idx, start_time_offset.microseconds))
+        measurements = file.get("ServerMeasurements")
+        previous = None
+        for m in measurements:
+            elapsed_sec = m.get("TCPInfo").get("ElapsedTime") / 1000000
+            rate_mbps = m.get("TCPInfo").get("BytesAcked") / elapsed_sec / 1000000 * 8
+            time = elapsed_sec + start_time_offset.microseconds / 1000000
+            if previous is not None:
+                dtime = m.get("TCPInfo").get("ElapsedTime") - previous.get("TCPInfo").get("ElapsedTime")
+                dbytes = m.get("TCPInfo").get("BytesAcked") - previous.get("TCPInfo").get("BytesAcked")
+                x_values.append(time)
+                y_values.append(dbytes / dtime * 8)
+                print("flow #{} time: {} bytesacked: {}".format(idx, time, m.get("TCPInfo").get("BytesAcked")))
+            previous = m
 
-                        axes[row][col].plot(avg_x, avg_y, "x:", label="avg. rate {}".format(i))
-                        axes[row][col].plot(rate_x, rate_y, "x-", label="rate {}".format(i))
+        print("flow #{} rates: {} {}".format(idx, x_values, y_values))
+        plot_data[idx] = (x_values, y_values)
 
-            # Make sure the ticks are always visible.
-            axes[row][col].tick_params(labelleft=True)
-            # Set custom step.
-            axes[row][col].yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(base=50.0))
-            # Set title and labels.
-            axes[row][col].set_title("{} - {} streams".format(root_dir, nstreams_dir))
-            axes[row][col].set_xlabel("Time (s)")
-            axes[row][col].set_ylabel("Throughput (Mbps)")
-            # Set legend location.
-            axes[row][col].legend(loc="upper left")
-            # Enable grid.
-            axes[row][col].grid()
-                    
+    all_x = []
+    for k in plot_data:
+        x, y = plot_data[k]
+        plt.plot(x, y, marker='o', label="flow #{}".format(k))
+        all_x = np.unique(np.concatenate((all_x, x)))
 
-    pylab.show()
+    all_y = []
+    for k in plot_data:
+        x, y = plot_data[k]
+        yi = np.interp(all_x, x, y, left=0, right=0)
+        all_y.append(yi)
+    
+    plt.plot(all_x, sum(all_y), marker='o', label="aggr. tp")
+    plt.legend()
+    plt.ylim(bottom=0)
+    plt.show()
+    
 
 if __name__ == "__main__":
     main()
